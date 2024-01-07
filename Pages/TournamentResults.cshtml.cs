15a38a54b5980ad29ba4d212ad45c2897357dbc2
Derrick Heinemann <itderrickh@gmail.com>
2024-01-07 16:31:03 -0600

    Adding a tournament results page

---
+++ Pages/TournamentResults.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CornDome.Pages
+{
+    public class TournamentResultsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
