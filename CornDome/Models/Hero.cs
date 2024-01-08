df0cdd3ff8ce2e7cacddc6d04fb0dc9312161f08
Derrick Heinemann <itderrickh@gmail.com>
2024-01-07 19:16:34 -0600

    Moving models out

--- CornDome/Models/Hero.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace CornDome.Models
-{
-    public class Hero
-    {
-        public required string Name { get; set; }
-        public required string Ability { get; set; }
-        public required string ImageUrl { get; set; }
-    }
-}
