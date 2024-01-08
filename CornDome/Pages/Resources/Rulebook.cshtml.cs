73a68df7bae8ce187386604b8b8ea13654c02dd3
Derrick Heinemann <itderrickh@gmail.com>
2024-01-07 21:14:46 -0600

    Adding resource pages

---
+++ CornDome/Pages/Resources/Rulebook.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CornDome.Pages.Resources
+{
+    public class RulebookModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
