32178c96b7b0912742bd903a04484b9ce426ed74
Derrick Heinemann <itderrickh@gmail.com>
2025-02-14 20:20:46 -0600

    Adding 2 sample decks on a new page

---
+++ CornDome/Pages/Resources/SampleDecks.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CornDome.Pages.Resources
+{
+    public class SampleDecksModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
