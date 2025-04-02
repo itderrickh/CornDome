71c7555404ed8442c1c4938659907185ae1913a4
Derrick Heinemann <itderrickh@gmail.com>
2025-04-02 16:29:06 -0500

    Emergency fix

--- CornDome.Repository/CardRepository.cs
+++ CornDome.Repository/CardRepository.cs
@@ -23,6 +23,14 @@ namespace CornDome.Repository
         public Card? GetCard(int id)
         {
             return context.Cards
+                .Include(card => card.Revisions)
+                .ThenInclude(cardRev => cardRev.CardImages)
+                .Include(card => card.Revisions)
+                .ThenInclude(cardRev => cardRev.CardSet)
+                .Include(card => card.Revisions)
+                .ThenInclude(cardRev => cardRev.Landscape)
+                .Include(card => card.Revisions)
+                .ThenInclude(cardRev => cardRev.CardType)
                 .FirstOrDefault(x => x.Id == id);
         }
 
