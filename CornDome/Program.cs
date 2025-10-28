28215dd9868a709a56464d41a086d961c5d0efe4
Derrick Heinemann <itderrickh@gmail.com>
2025-10-27 19:18:33 -0500

    Removing ef sql logs

--- CornDome/Program.cs
+++ CornDome/Program.cs
@@ -105,6 +105,7 @@ namespace CornDome
                 //app.UseExceptionHandler("/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
+                builder.Logging.AddFilter("Microsoft.EntityFrameworkCore.Database.Command", LogLevel.None);
             }
 
             //app.UseStatusCodePagesWithRedirects("/Errors/Error{0}");
