497a48409bc190b9ebee36ed8875b652cc4826b1
Derrick Heinemann <itderrickh@gmail.com>
2025-11-07 18:48:42 -0600

    Adding a backlog page

---
+++ CornDome/Pages/Backlog.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CornDome.Pages
+{
+    public class BacklogModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
