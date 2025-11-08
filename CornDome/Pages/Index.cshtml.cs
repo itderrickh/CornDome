b15e6fb9605edec2d80fd2c3869693c4aa91002c
Derrick Heinemann <itderrickh@gmail.com>
2025-11-07 18:31:00 -0600

    Removing legacy articles

--- CornDome/Pages/Index.cshtml.cs
+++ CornDome/Pages/Index.cshtml.cs
@@ -1,5 +1,3 @@
-using CornDome.ContentManager;
-using CornDome.Models.Articles;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace CornDome.Pages
@@ -7,22 +5,10 @@ namespace CornDome.Pages
     public class IndexModel(IConfiguration config, ILogger<IndexModel> logger) : PageModel
     {
         private readonly ILogger<IndexModel> _logger = logger;
-        private readonly ContentLoader _contentLoader = new();
         private readonly IConfiguration _config = config;
-        public IEnumerable<Article> Articles = new List<Article>();
-
-        public string GetFinalDirectory(Article article) => $"Article?page={article.Location.Replace(".md", "")}";
 
         public void OnGet()
         {
-            var articlePath = _config.GetValue<string>("ContentStore:Articles");
-            if (articlePath == null)
-            {
-                _logger.LogError($"Configuration not set: \"ContentStore:Articles\"");
-                return;
-            }
-
-            Articles = _contentLoader.GetContentFromStore(articlePath, "/EmbeddedImages");
         }
     }
 }
