6ebb198ffa8a52a1441092cbca522423588d01e1
Derrick Heinemann <itderrickh@gmail.com>
2025-12-02 14:36:01 -0600

    Updating the deck page

---
+++ CornDome/wwwroot/js/deck.js
@@ -0,0 +1,16 @@
+﻿document.getElementById("saveButton").onclick = async function () {
+    var query = new URLSearchParams(document.location.search);
+    var deck = query.get("deck");
+    var db = new DecksDatabase();
+
+    try {
+        await db.open();
+
+        const newDeck = { name: "New deck " + getFormattedDateTime(), content: deck };
+        const addMessage = await db.addDeck(newDeck);
+
+        alert(addMessage);
+    } catch (ex) {
+        alert("Error adding deck: ", ex);
+    }
+}
