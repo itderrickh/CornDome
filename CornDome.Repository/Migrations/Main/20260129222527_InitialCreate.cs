b864e0835c7e67f6e435674e7303f0b29d4af28b
Derrick Heinemann <itderrickh@gmail.com>
2026-01-29 16:40:13 -0600

    Fixing a migration

--- CornDome.Repository/Migrations/Main/20260129222527_InitialCreate.cs
+++ CornDome.Repository/Migrations/Main/20260129222527_InitialCreate.cs
@@ -51,16 +51,16 @@ namespace CornDome.Repository.Migrations.Main
                     NormalizedUserName = table.Column<string>(type: "TEXT", nullable: true),
                     Email = table.Column<string>(type: "TEXT", nullable: true),
                     NormalizedEmail = table.Column<string>(type: "TEXT", nullable: true),
-                    EmailConfirmed = table.Column<bool>(type: "INTEGER", nullable: true),
+                    EmailConfirmed = table.Column<bool>(type: "INTEGER", nullable: true, defaultValue: 0),
                     PasswordHash = table.Column<string>(type: "TEXT", nullable: true),
                     SecurityStamp = table.Column<string>(type: "TEXT", nullable: true),
                     ConcurrencyStamp = table.Column<string>(type: "TEXT", nullable: true),
                     PhoneNumber = table.Column<string>(type: "TEXT", nullable: true),
-                    PhoneNumberConfirmed = table.Column<bool>(type: "INTEGER", nullable: true),
-                    TwoFactorEnabled = table.Column<bool>(type: "INTEGER", nullable: true),
+                    PhoneNumberConfirmed = table.Column<bool>(type: "INTEGER", nullable: true, defaultValue: 0),
+                    TwoFactorEnabled = table.Column<bool>(type: "INTEGER", nullable: true, defaultValue: 0),
                     LockoutEnd = table.Column<DateTimeOffset>(type: "TEXT", nullable: true),
-                    LockoutEnabled = table.Column<bool>(type: "INTEGER", nullable: true),
-                    AccessFailedCount = table.Column<int>(type: "INTEGER", nullable: true)
+                    LockoutEnabled = table.Column<bool>(type: "INTEGER", nullable: true, defaultValue: 0),
+                    AccessFailedCount = table.Column<int>(type: "INTEGER", nullable: true, defaultValue: 0)
                 },
                 constraints: table =>
                 {
