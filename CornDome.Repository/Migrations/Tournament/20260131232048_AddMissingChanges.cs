1ae44bdbfd1a30a247de2b5712fb08d78f867d14
Derrick Heinemann <itderrickh@gmail.com>
2026-01-31 17:20:59 -0600

    Adding missing migration changes

---
+++ CornDome.Repository/Migrations/Tournament/20260131232048_AddMissingChanges.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CornDome.Repository.Migrations.Tournament
+{
+    /// <inheritdoc />
+    public partial class AddMissingChanges : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "RoundEnd",
+                table: "round",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "RoundStart",
+                table: "round",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "RoundEnd",
+                table: "round");
+
+            migrationBuilder.DropColumn(
+                name: "RoundStart",
+                table: "round");
+        }
+    }
+}
