2b0f7efaba0d52702914c4124f79b7ace844c497
Derrick Heinemann <itderrickh@gmail.com>
2026-02-23 14:26:35 -0600

    Save fix 2

--- CornDome/wwwroot/js/deckbuilder.handlers.js
+++ CornDome/wwwroot/js/deckbuilder.handlers.js
@@ -284,12 +284,21 @@ document.getElementById("tryoutButton").onclick = function () {
 document.getElementById("saveButton").onclick = async function () {
     var query = new URLSearchParams(document.location.search);
     var deck = query.get("deck");
+    var gzdeck = query.get("gzdeck");
     var db = new DecksDatabase();
+
+    var deckToSave = "";
+    if (deck !== '') {
+        deckToSave = deck;
+    }
+    else {
+        deckToSave = gzdeck;
+    }
 
     try {
         await db.open();
 
-        const newDeck = { name: "New deck " + getFormattedDateTime(), content: deck };
+        const newDeck = { name: "New deck " + getFormattedDateTime(), content: deckToSave };
         const addMessage = await db.addDeck(newDeck);
 
         alert(addMessage);
