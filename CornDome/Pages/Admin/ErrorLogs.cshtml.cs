8d1b458a0eca673a81f508cb7ca0975c3bd3bc4f
Derrick Heinemann <itderrickh@gmail.com>
2026-07-31 14:13:02 -0500

    Fixing permissions

--- CornDome/Pages/Admin/ErrorLogs.cshtml.cs
+++ CornDome/Pages/Admin/ErrorLogs.cshtml.cs
@@ -1,9 +1,11 @@
 using CornDome.Models;
 using CornDome.Repository;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace CornDome.Pages.Admin
 {
+    [Authorize(Policy = "admin")]
     public class ErrorLogsModel(ILogEntryRepository repository) : PageModel
     {
         public IEnumerable<LogEntry> Logs { get; set; } = [];
