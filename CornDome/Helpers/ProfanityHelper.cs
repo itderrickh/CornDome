ccbd6ff6d671d5d6fbe99aa41fc37d9d0dc5b888
Derrick Heinemann <itderrickh@gmail.com>
2026-09-21 22:28:13 -0500

    Profanity detection

---
+++ CornDome/Helpers/ProfanityHelper.cs
@@ -0,0 +1,18 @@
+﻿
+using DotnetBadWordDetector;
+
+namespace CornDome.Helpers
+{
+    public static class ProfanityHelper
+    {
+        private static readonly ProfanityDetector Detector = new();
+
+        public static string RelieveTheProfane(string text)
+        {
+            if (string.IsNullOrWhiteSpace(text))
+                return string.Empty;
+
+            return Detector.MaskProfanity(text, '*');
+        }
+    }
+}
